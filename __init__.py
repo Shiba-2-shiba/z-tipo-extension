fc1426c8f1855195ffdc3ceb9f26f7e21cdca039
Shiba*2 <135475967+Shiba-2-shiba@users.noreply.github.com>
2025-08-25 03:03:56 +0900

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,22 +1,23 @@
-# Import mappings from each node module
+
+from .nodes.tipo_preprocessor import NODE_CLASS_MAPPINGS as preprocessor_mappings, NODE_DISPLAY_NAME_MAPPINGS as preprocessor_display_mappings
 from .nodes.tipo_wildcard import NODE_CLASS_MAPPINGS as wildcard_mappings, NODE_DISPLAY_NAME_MAPPINGS as wildcard_display_mappings
 from .nodes.tipo_natural_language import NODE_CLASS_MAPPINGS as nl_mappings, NODE_DISPLAY_NAME_MAPPINGS as nl_display_mappings
 from .nodes.tipo_simple import NODE_CLASS_MAPPINGS as simple_mappings, NODE_DISPLAY_NAME_MAPPINGS as simple_display_mappings
 
-# Combine all mappings into one
 NODE_CLASS_MAPPINGS = {
+    **preprocessor_mappings,
     **wildcard_mappings,
     **nl_mappings,
     **simple_mappings,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
+    **preprocessor_display_mappings,
     **wildcard_display_mappings,
     **nl_display_mappings,
     **simple_display_mappings,
 }
 
-# Expose the combined mappings
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS"]
 
 print("------------------------------------------")
